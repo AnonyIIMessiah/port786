ecdc77379a439c6ccd0a2f44c0f551350a023ac4
AnonyIIMessiah <79109596+AnonyIIMessiah@users.noreply.github.com>
2022-09-06 13:47:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// typing annimation
+var typed = new Typed(".typing",{
+strings:["Web Designer" , "Web developer", "Competetive Coder", "Quick Learner" , "Learning enthusiast"],
+typeSpeed:60,
+BackSpeed:60,
+loop:true
+});
